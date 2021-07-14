b71f71b6e3887e01702fa7bb1e6c0ddbcce8798e
Aleksey <Aleksey.bortnik91@gmail.com>
2021-07-14 18:33:17 +0300

    settings added

---
+++ js-tasks/task1.js
@@ -0,0 +1,12 @@
+function sum() {
+  const value = Object.values(arguments).reduce((acc, next) => {
+    if (typeof next === "number") {
+      return acc + next;
+    } else {
+      return acc;
+    }
+  }, 0);
+  return value;
+}
+
+console.log(sum("324", 1, 2, 3, [], "ffffff"));
