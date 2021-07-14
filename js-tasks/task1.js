2dd3290b3203e4c591ebd53218059b85c544a086
Aleksey <Aleksey.bortnik91@gmail.com>
2021-07-14 19:34:31 +0300

    auto fix

--- js-tasks/task1.js
+++ js-tasks/task1.js
@@ -1,12 +1,11 @@
 function sum() {
   const value = Object.values(arguments).reduce((acc, next) => {
-    if (typeof next === "number") {
+    if (typeof next === 'number') {
       return acc + next;
-    } else {
-      return acc;
     }
+    return acc;
   }, 0);
   return value;
 }
 
-console.log(sum("324", 1, 2, 3, [], "ffffff"));
+console.log(sum('324', 1, 2, 3, [], 'ffffff'));
