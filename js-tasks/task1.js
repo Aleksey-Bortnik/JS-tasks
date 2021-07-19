08a60042fbfa7c2e7745e54a7edcd0623d03661e
Aleksey <Aleksey.bortnik91@gmail.com>
2021-07-19 13:26:28 +0300

    undefined check added

--- js-tasks/task1.js
+++ js-tasks/task1.js
@@ -1,4 +1,7 @@
 function summ() {
+  if (!Object.values(arguments).length) {
+    return null;
+  }
   return Object.values(arguments).reduce(
     (acc, next) =>
       typeof next !== 'boolean' && !Number.isNaN(+next) ? acc + +next : acc,
@@ -7,6 +10,9 @@ function summ() {
 }
 
 function summAdvanced() {
+  if (!Object.values(arguments).length) {
+    return null;
+  }
   return Object.values(arguments).reduce((acc, next) => {
     if (typeof next !== 'boolean' && !Number.isNaN(+next)) {
       return acc + +next;
@@ -24,17 +30,22 @@ function isValueExists(a) {
   if (a === null || typeof a === 'undefined') {
     return false;
   }
+
   return true;
 }
 
 function callWithFunctionResult(funct1, funct2) {
+  if (!funct1 || !funct2) {
+    return null;
+  }
   return funct1(funct2());
 }
 
 function callWhileStringIsNotEmpty(string, func) {
-  if (!string || typeof string === 'number') {
+  if (!string || typeof string === 'number' || !func) {
     return;
   }
+
   func(string);
   const tmp = string.slice(0, -1);
   callWhileStringIsNotEmpty(tmp, func);
